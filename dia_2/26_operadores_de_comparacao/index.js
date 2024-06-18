57e9e2fb0a13702361c00424573195ff9757406b
rafakroz <rafakroz@gmail.com>
2024-06-18 14:57:18 -0300

    Aulas 26 e 27 do dia 2

---
+++ dia_2/26_operadores_de_comparacao/index.js
@@ -0,0 +1,29 @@
+console.log('Seção 3: Dia 2 - Aula 26');
+console.log('Operadores de Comparação');
+console.log('-----------------------');
+
+/*
+Operadores de Comparação
+
+*/
+
+let num = 3;
+
+let str = 'Rafael'
+
+console.log(num > 0);
+console.log(num < 0);
+console.log(num >= 0);
+console.log(num <= 3);
+
+// idêntico
+console.log(num === 3);
+console.log('Igualdade: ' + (num === 3));
+
+console.log(num === str);
+
+// não é idêntico
+console.log(num !== 3);
+console.log('Igualdade: ' + (num !== 3));
+
+console.log(num !== str);
---
+++ dia_2/27_operadores_de_igualdade/index.js
@@ -0,0 +1,33 @@
+console.log('Seção 3: Dia 2 - Aula 27');
+console.log('Operadores de Igualdade');
+console.log('-----------------------');
+
+/*
+Operadores de Igualdade
+
+===
+Verifica a igualdade de valor e tipo
+
+*/
+
+// 2 números
+let num1 = 1;
+let num2 = 1;
+
+console.log(num1 === num2); // true
+
+//-------------------------
+
+// 1 string e 1 número
+let num3 = '1';
+let num4 = 1;
+
+console.log(num3 === num4); //false
+
+//-------------------------
+
+// 1 string e 1 número, mas, os valores são iguais
+let num5 = '1';
+let num6 = 1;
+
+console.log(num3 == num4); // true
