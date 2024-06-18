978f9d9ece000051f3a11ab63a04e6faad2a51e4
Rafael Queiroz <rafakroz@gmail.com>
2024-06-17 21:36:43 -0300

    Aulas 15 e 17 da seção 2

---
+++ dia_1/15_constantes/index.js
@@ -0,0 +1,28 @@
+// Seção 2
+// Dia 1: Aula 15 - Constantes 
+
+console.log('Javascript funcionando!');
+
+// valor definido para a variável
+
+let price = 50;
+
+console.log(price);
+
+// Novo valor definido
+
+price = 30;
+
+console.log(price);
+
+// ---------------------------------------------
+
+// Defininindo uma constante
+
+const altura = 160;
+
+console.log(altura);
+
+// Alteração de valor, resultando em erro
+
+altura = 170;
---
+++ dia_1/17_tipos_primitivos/index.js
@@ -0,0 +1,32 @@
+// Seção 2
+// Dia 1: Aula 15 - Constantes 
+
+console.log('Javascript funcionando!');
+console.log('Seção 2 - Aula 17');
+console.log('-----------------------');
+
+/*
+Tipos Primitivos
+
+String
+Número
+Boolean
+Undefined
+Null
+
+*/
+
+let itemName = 'Pen';
+console.log('Nome do Produto:', itemName);
+
+let itemPrice = 3;
+console.log('Preço do Produto:', itemPrice);
+
+let itemAvalable = true;
+console.log('Produto disponível:', itemAvalable);
+
+let itemColor;
+console.log('Cor do Produto:', itemColor);
+
+itemPrice = null
+console.log('Preço do Produto:', itemPrice);
