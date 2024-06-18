441f001677e44df9494f9200a03d3acab091898d
rafakroz <rafakroz@gmail.com>
2024-06-18 16:54:23 -0300

    Aulas 32 e 33 do dia 2

---
+++ dia_2/32_if_else/index.js
@@ -0,0 +1,26 @@
+console.log('Seção 3: Dia 2 - Aula 32');
+console.log('Declarações condicionais: if else');
+console.log('-----------------------');
+
+/*
+
+*/
+
+let driverSpeed = 80;
+let limite = 110;
+
+if (driverSpeed > limite) {
+
+    console.log('O motorista está acima do limite de velocidade!');
+
+}
+else if (driverSpeed > 40 && driverSpeed <= limite) {
+
+    console.log('O motorista está dentro do limite de velocidade')
+
+}
+else {
+
+    console.log('O motorsita está muito devagar!');
+
+}
---
+++ dia_2/33_switch_case/index.js
@@ -0,0 +1,23 @@
+console.log('Seção 3: Dia 2 - Aula 33');
+console.log('Switch e Case');
+console.log('-----------------------');
+
+/*
+
+*/
+
+let airportName = 'MCO';
+
+switch (airportName) {
+    case 'MCO':
+        console.log('Orlando');
+        break;
+    case 'JFK':
+        console.log('John F. Kennedy');
+        break;
+    case 'SEA':
+        console.log('Seattle');
+        break;
+    default:
+        console.log('Aeroporto não localizado');
+    }
