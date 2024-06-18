82b21bf18fae17d3d376d3b9a5fa5e815ad034c3
rafakroz <rafakroz@gmail.com>
2024-06-18 14:21:32 -0300

    Aulas 24 e 25 do dia 2

---
+++ dia_2/24_operadores_artitmetricos/index.js
@@ -0,0 +1,50 @@
+console.log('Seção 3: Dia 2 - Aula 24');
+console.log('Operadores Aritmétricos');
+console.log('-----------------------');
+
+/*
+Operadores Artimétricos
+
+*/
+
+let num1 = 3;
+let num2 = 2;
+
+console.log(num1 + num2);
+
+console.log(num1 - num2);
+
+console.log(num1 * num2);
+
+console.log(num1 / num2);
+
+console.log(num1 % num2);
+
+console.log(num1 ** num2);
+
+let total1 = num1 + num2;
+
+let total2 = num1 + num2;
+
+console.log(total1);
+
+// incremento ++
+
+console.log('Incremento antes: ' + ++total1);
+
+// Altera o valor, mas, não exibe no console
+console.log('Incremento depois: ' + total2++);
+// Exibe o valor alterado, após o incremento
+console.log(total2);
+
+// decremento --
+
+let total3 = num1 + num2;
+
+console.log('Decremento antes: ' + --total3);
+
+let total4 = num1 + num2;
+
+console.log('Decremento depois: ' + total4--);
+
+console.log(total4);
---
+++ dia_2/25_operadores_de_atribuicao/index.js
@@ -0,0 +1,46 @@
+console.log('Seção 3: Dia 2 - Aula 25');
+console.log('Operadores de Atribuição');
+console.log('-----------------------');
+
+/*
+Operadores de atribuição
+
+Por exemplo:
+
+num = num + 1 poderia ser num += 1
+
+*/
+
+
+// + 
+
+let num1 = 20;
+
+num1 += 10;
+
+console.log(num1);
+
+// - 
+
+let num2 = 20;
+
+num2 -= 10;
+
+console.log(num2);
+
+// *
+
+let num3 = 20;
+
+num3 *= 10;
+
+console.log(num3);
+
+// /
+
+let num4 = 20;
+
+num4 /= 10;
+
+console.log(num4);
+
