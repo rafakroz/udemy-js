28cddbbcb74207532941141fdb6a10fea74bc601
Rafael Queiroz <rafakroz@gmail.com>
2024-06-17 21:09:49 -0300

    Aulas 13 e 14 da seção 2

---
+++ dia_1/14_variaveis/index.js
@@ -0,0 +1,13 @@
+// Seção 2
+// Dia 1: Aula 14 - Variáveis 
+
+console.log('Javascript funcionando!');
+
+// Não iniciar com número
+// Demais termos iniciando em caixa alta
+
+let firstName = 'Rafael';
+let lastName = 'Queiroz';
+
+console.log(firstName);
+console.log(lastName);
