b3b3f464d25969e69c33bf17ee5f7843e7a25703
Rafael Trabalho <rafakroz@gmail.com>
2024-06-19 09:39:09 -0300

    Desafio 1 concluído

---
+++ desafios_JavaScript/desafio_1/index.js
@@ -0,0 +1,15 @@
+let nome = 'Rafael';
+
+let generoMusical = 'Rock';
+
+let time = 'Flamengo';
+
+let cor = 'Preto';
+
+document.getElementById('nomePessoa').innerHTML = nome;
+
+document.getElementById('estiloPessoa').innerHTML = generoMusical;
+
+document.getElementById('timePessoa').innerHTML = time;
+
+document.getElementById('corPessoa').innerHTML = cor;
