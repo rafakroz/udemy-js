e6378cef3de4c6bcb6e013c243f0050fe08e2b04
Rafael Trabalho <rafakroz@gmail.com>
2024-06-19 08:02:47 -0300

    Aulas 34 e 35 do dia 2

---
+++ dia_2/34_for_loop/index.js
@@ -0,0 +1,17 @@
+console.log('Seção 3: Dia 2 - Aula 34');
+console.log('For Loop');
+console.log('-----------------------');
+
+/*
+for ( variável; condição; incremento)
+*/
+
+for (i = 1; i <= 10; i++){
+    console.log('Número ', i)
+};
+
+console.log('---------');
+
+for (i = 1; i <=20; i++){
+    console.log('Número ', i)
+};
---
+++ dia_2/35_while_loop/index.js
@@ -0,0 +1,22 @@
+console.log('Seção 3: Dia 2 - Aula 35');
+console.log('While Loop');
+console.log('-----------------------');
+
+/*
+for ( variável; condição; incremento)
+*/
+
+/*
+for (i = 1; i <= 10; i++){
+    console.log('Número ', i)
+};
+*/
+
+i = 1; // variável externa
+
+while (i <=10) {
+
+    console.log('(While) Número ', i);
+
+    i++;
+}
