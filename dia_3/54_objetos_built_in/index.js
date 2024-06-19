e625f210a0cd7e16e708ebc4ed9c577eb7782fe4
Rafael Trabalho <rafakroz@gmail.com>
2024-06-19 15:41:33 -0300

    Aulas 54 e 55 do dia 3

---
+++ dia_3/54_objetos_built_in/index.js
@@ -0,0 +1,14 @@
+console.log('Seção 4: Dia 3 - Aula 54');
+console.log('Objetos Built in');
+console.log('-----------------------');
+
+/*
+*/
+
+console.log(Math.max(1, 3, 15, 2, 7, 4));
+
+console.log(Math.min(1, 3, 15, 2, 7, 4));
+
+console.log(Math.round(5.3));
+
+console.log(Math.round(5.5));
---
+++ dia_3/55_string/index.js
@@ -0,0 +1,26 @@
+console.log('Seção 4: Dia 3 - Aula 55');
+console.log('Strings');
+console.log('-----------------------');
+
+/*
+*/
+
+let message = 'Boa tarde! Meu nome é Rafael!';
+
+// Mensagem inicia com xxx
+console.log(message.startsWith('Meu'));
+
+// Mensagem inicia com xxx
+console.log(message.startsWith('Boa'));
+
+// Verificando tamanho da mensagem
+console.log(message.length);
+
+// xxx existe na mensagem?
+console.log(message.includes('nome'));
+
+// Mensagem finaliza com xxx
+console.log(message.endsWith('Rafael!'));
+
+// Alterar mensagem para caixa alta
+console.log(message.toUpperCase());
