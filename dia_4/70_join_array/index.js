7f85835ef143a7fe690ab53616f3d21f55ffc04d
Rafael Trabalho <rafakroz@gmail.com>
2024-06-20 11:23:53 -0300

    Aulas 70 e 71 do dia 4

---
+++ dia_4/70_join_array/index.js
@@ -0,0 +1,23 @@
+console.log('Seção 5: Dia 4 - Aula 70');
+console.log('Utilizando o Join Array');
+console.log('-----------------------');
+
+/*
+*/
+
+let clients = ['Rafael', 'Fred', 'Suzy', 'Nina'];
+
+console.log(clients);
+
+// Converte a array em string, e adiciona a informação definida
+
+let clientsJoin1 = clients.join('x');
+
+console.log(clientsJoin1);
+
+let clientsJoin2 = clients.join(', ');
+
+console.log(clientsJoin2);
+// ------------
+
+
---
+++ dia_4/71_ordenando_array/index.js
@@ -0,0 +1,23 @@
+console.log('Seção 5: Dia 4 - Aula 71');
+console.log('Rervertendo a sua Array');
+console.log('-----------------------');
+
+/*
+*/
+
+let clients1 = ['D', 'B', 'A', 'C'];
+
+// sort para ordenação crescente
+clients1.sort();
+
+console.log(clients1);
+
+let clients2 = [4, 2, 1, 3];
+
+// reverse inverte a ordem do itens.
+
+clients2.reverse();
+
+console.log(clients2);
+
+
