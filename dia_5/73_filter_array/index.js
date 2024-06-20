ff88f4427efd850e93bd84427c6ec2636668971e
Rafael Trabalho <rafakroz@gmail.com>
2024-06-20 16:53:56 -0300

    Aula 73 do  dia 5

---
+++ dia_5/73_filter_array/index.js
@@ -0,0 +1,33 @@
+console.log('Seção 6: Dia 5 - Aula 79');
+console.log('Iniciando com as funções');
+console.log('-----------------------');
+
+/*
+*/
+
+
+// Function Declaration
+
+function movie() {
+
+    console.log('O Senhor dos Anéis');
+
+};
+
+// invocando a função
+
+movie();
+
+// Function Expression
+
+const car = function() {
+
+    console.log('BMW X9');
+
+};
+
+car();
+
+const truck = car;
+
+truck();
