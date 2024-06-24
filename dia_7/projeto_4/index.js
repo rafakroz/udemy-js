67619f1f57417c93d1624693aff3cc4cf890d8ce
Rafael Trabalho <rafakroz@gmail.com>
2024-06-24 14:08:15 -0300

    Número de tentativas implementado

--- dia_7/projeto_4/index.js
+++ dia_7/projeto_4/index.js
@@ -1,6 +1,6 @@
 let computerNumber;
-
 let userNumbers = [];
+let tentativas = 0;
 
 function init() {
     // gerando o número de 1 a 100
@@ -23,36 +23,56 @@ function compareNumbers() {
     if (userNumber > computerNumber && (Math.abs((userNumber - computerNumber)) >= 20)) {
         document.querySelector('#textOutput').innerHTML = 'Seu número é muito alto!';
         document.querySelector('#inputBox').value = '';
+        // A cada if/else if, a tentativa é incrementada
+        tentativas++
+        // Em seguida, insere o número no campo
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
     else if (userNumber > computerNumber && (Math.abs((userNumber - computerNumber)) >= 10)){
         document.querySelector('#textOutput').innerHTML = 'Seu número está alto!';
         document.querySelector('#inputBox').value = '';
+        tentativas++
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
     else if (userNumber > computerNumber && (Math.abs((userNumber - computerNumber)) == 1)){
         document.querySelector('#textOutput').innerHTML = 'Seu número está bem próximo!';
         document.querySelector('#inputBox').value = '';
+        tentativas++
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
     else if (userNumber > computerNumber && (Math.abs((userNumber - computerNumber)) < 10)){
         document.querySelector('#textOutput').innerHTML = 'Seu número está próximo!';
         document.querySelector('#inputBox').value = '';
+        tentativas++
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
     else if (userNumber < computerNumber && (Math.abs((userNumber - computerNumber)) >= 20)) {
         document.querySelector('#textOutput').innerHTML = 'Seu número é muito baixo!';
         document.querySelector('#inputBox').value = '';
+        tentativas++
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
     else if (userNumber < computerNumber && (Math.abs((userNumber - computerNumber)) >= 10)){
         document.querySelector('#textOutput').innerHTML = 'Seu número está baixo!';
         document.querySelector('#inputBox').value = '';
+        tentativas++
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
     else if (userNumber < computerNumber && (Math.abs((userNumber - computerNumber)) == 1)){
         document.querySelector('#textOutput').innerHTML = 'Seu número está bem próximo!';
         document.querySelector('#inputBox').value = '';
+        tentativas++
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
     else if (userNumber < computerNumber && (Math.abs((userNumber - computerNumber)) < 10)){
         document.querySelector('#textOutput').innerHTML = 'Seu número está próximo!';
         document.querySelector('#inputBox').value = '';
+        tentativas++
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
     else {
         document.querySelector('#textOutput').innerHTML = 'Parabéns! Você acertou!';
+        tentativas++
+        document.querySelector('#attempts').innerHTML = tentativas;
     }
 }
