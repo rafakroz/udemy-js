eb1ebf53b2d8796e497c7d22512700f2bfd71a6f
Rafael Trabalho <rafakroz@gmail.com>
2024-06-24 09:31:47 -0300

    Armazenando os numeros na array

--- dia_7/projeto_4/index.js
+++ dia_7/projeto_4/index.js
@@ -1,8 +1,21 @@
 let computerNumber;
 
+let userNumbers = [];
+
 function init() {
     // gerando o número de 1 a 100
     // Por iniciar com 0, +1 determinar que irá até 100
     computerNumber = Math.floor(Math.random() * 100 + 1);
     console.log(computerNumber)
 };
+
+function compareNumbers() {
+    // capturando o número selecionado no input
+    const userNumber = Number(document.getElementById('inputBox').value);
+
+    // inserindo o numero selecionado no array userNumbers
+    userNumbers.push(' ' + userNumber);
+
+    // o número é inserido no array e exibido no campo de chutes
+    document.getElementById('guesses').innerHTML = userNumbers;
+}
