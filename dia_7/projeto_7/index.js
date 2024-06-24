8b5dc351751cb2044695ae3bf96ad7499666c4a1
Rafael Trabalho <rafakroz@gmail.com>
2024-06-24 09:04:40 -0300

    Projeto 4 iniciado

---
+++ dia_7/projeto_7/index.js
@@ -0,0 +1,8 @@
+let computerNumber;
+
+function init() {
+    // gerando o número de 1 a 100
+    // Por iniciar com 0, +1 determinar que irá até 100
+    computerNumber = Math.floor(Math.random() * 100 + 1);
+    console.log(computerNumber)
+};
